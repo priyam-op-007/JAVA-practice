fa7b28871c482191588b58c4c3c65f26852e6daf
Priyaranjan Mishra <priyaranjanmishra000@gmail.com>
2022-08-01 20:00:47 +0530

    Create factorial by for loop.java

---
+++ factorial by for loop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class factorial_by_for_loop {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = reader.nextInt();
+        int fact = 1;
+        for (int i=1;i <= num; i++) {
+            fact = fact*i;
+        }
+        System.out.println("the factorial is " + fact);
+    }
+}
+
+
+
+solution - 
+enter a number
+5
+the factorial is 120
