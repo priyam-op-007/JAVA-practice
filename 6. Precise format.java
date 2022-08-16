c912c5c82dba4ce2505d50032be19bc90de848b5
Priyaranjan Mishra <priyaranjanmishra000@gmail.com>
2022-08-16 19:57:12 +0530

    Update 6. Precise format.java

--- 6. Precise format.java
+++ 6. Precise format.java
@@ -1,5 +1,6 @@
 There are times when your answer is a floating point that contains undesired amount of digits after decimal. Here, we'll learn how to get a precise answer out of a floating number. You are given two floating numbers a and b. You need to output a/b and decimal precision of a/b upto 3 places after decimal point.
 Note: You may use System.out.format()
+	
 
 Example 1:
 
@@ -73,4 +74,4 @@ class GFG {
 		
 	}
 }
-  // } Driver Code Ends
+  //  Driver Code Ends
